2580913d71693c9ec2a3804a38d3e39779eab8a6
Bhaskar Jha <47430433+bhaskar2840@users.noreply.github.com>
2021-04-13 23:03:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const express = require("express");
 const ejs = require ("ejs");
 const mongoose = require("mongoose");
 
-// const encrypt = require("mongoose-encryption"); // npm i mongoose-encryption
+// const encrypt = require("mongoose-encryption");  // npm i mongoose-encryption
+//const md5=require("md5");  //for the hashing.
+//const bcrypt=recquire("bcrypt"); // another hasing function.
+//const saltRounds = 10;
 
-//const md5=require("md5"); //for the hashing.
+const passport = require("passport");
+const session=require("express-session");
+const passportLocalMongoose= require("passport-local-mongoose");
 
-const bcrypt=recquire("bcrypt"); // another hasing function.
-const saltRounds = 10;
  
 
 
@@ -22,7 +25,18 @@ app.set("view engine","ejs");
 
 app.use(express.urlencoded({extended:true}));
 
+app.use(session({
+    secret:"our little secret.",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true},{ useUnifiedTopology: true});
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema= new mongoose.Schema({ // for encrption new type of schema is created
     email:String,
@@ -38,9 +52,14 @@ const userSchema= new mongoose.Schema({ // for encrption new type of schema is c
 // encrptedFields will work and encrpt when the save option is trigered and decrpt when find option is shown.
 
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/",function(req,res){
@@ -55,42 +74,48 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated()){
+      res.render("secrets");
+    } else {
+      res.redirect("/login");
+    }
+  });
+  
+  app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+  });
 
 
-app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-        const newUser = new User({
-            email:req.body.username,
-            password:hash // this will do the hashing in the password.
-        });
-        newUser.save(function(err){  // this will trigger the encryption 
-            if(err){
-                console.log(err);
-            }
-            else{res.render("secrets");}
-        })
-
-    });
-    
+app.post("/register",function(req,res){ 
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });    
 });
 
 app.post("/login",function(req,res){
 
-    const username=req.body.username;
-    const password = req.body.password; // used to match the hash.
-
-    User.findOne({email:username},function(err,foundUser)
-    {
-        if (err){console.log(err);}
-        else{
-            if(foundUser){
-                bcrypt.compare(password,foundUser,password,function(err,result){
-                    if (result === true){ res.render("secrets");} })
-               
-            }
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+    
+      req.login(user, function(err){
+        if (err) {
+          console.log(err);
+        } else { passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets");
+          });
         }
-        
-    });
+      });
     
 
 }); 
